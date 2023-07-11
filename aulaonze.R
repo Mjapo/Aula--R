3182760d60db836a20bb89978601d41165cf6885
marcelo ishida takeya <96395400+Mjapo@users.noreply.github.com>
2023-07-10 23:56:45 -0300

    Add files via upload

---
+++ aulaonze.R
@@ -0,0 +1,52 @@
+###  instalacao do tidyverse 
+
+install.packages("tidyverse")
+library(tidyverse)
+
+
+# Aqui você pode começar a usar as funções desses pacotes para analisar seus dados
+
+####dplyr: O dplyr fornece uma gramática de manipulação de dados que facilita a seleção,
+#filtragem, transformação, agregação e junção de conjuntos de dados. Ele oferece funções intuitivas e eficientes
+#, como filter(), select(), mutate(), group_by(), entre outras.
+
+####ggplot2: O ggplot2 é um pacote poderoso para criação de gráficos estatísticos. 
+#Ele permite a construção de visualizações elegantes e personalizadas, seguindo a abordagem de 
+#"camadas" (layers). O ggplot2 é baseado em uma gramática de gráficos, facilitando a 
+#criação de gráficos complexos com pouco código.
+
+####tidyr: O tidyr é usado para transformar e reorganizar dados. Ele fornece funções como gather() 
+#e spread() para converter dados entre os formatos "long" e "wide". O tidyr é útil 
+#quando você precisa reformatar seus dados para análise ou visualização específicas.
+
+####readr: O readr é um pacote para importação eficiente de dados em R. Ele oferece funções rápidas
+#e flexíveis para ler arquivos de texto delimitados, arquivos CSV e outros formatos de dados. O readr 
+#é especialmente útil quando você precisa importar grandes conjuntos de dados de forma eficiente.
+
+####stringr" faz parte do Tidyverse e é um pacote desenvolvido para manipulação de strings (cadeias de caracteres)
+##no R. Ele fornece um conjunto de funções simples e intuitivas para trabalhar com strings,
+##facilitando tarefas comuns, como manipulação, extração, substituição e combinação de texto.
+
+####readr" é um dos pacotes do Tidyverse e é usado para a leitura eficiente de dados no R. 
+##Ele fornece funções rápidas e flexíveis para importar dados de diferentes formatos, 
+##como arquivos de texto delimitados, arquivos CSV, arquivos de largura fixa e outros formatos comuns.
+
+
+#### codigo da instalacao dos pacotes 
+
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("readr")
+
+### codigo para carregamento das bibliotecas 
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(stringr)
+library(readr)
+
+
+
